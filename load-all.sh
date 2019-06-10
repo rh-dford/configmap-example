f97b1ab03792c3363172e77a7d5151a052da6c2f
root <root@egs-ocp-master1.lab.rh.ecore.af.smil.mil>
2019-06-10 11:21:26 -0600

    adding build config

--- load-all.sh
+++ load-all.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 oc new-project configmap-example
+oc create -f node-app-build.json
 oc create -f configmap-example.json
 oc create -f node-app-service.json
 oc create -f node-app-deployment.json
